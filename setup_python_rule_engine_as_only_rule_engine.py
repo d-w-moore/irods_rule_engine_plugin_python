d196832fb7a7ceeedb470d17d01a6dbd74c2b094
d-w-moore <dmoore@renci.org>
2021-05-27 01:11:17 -0400

    correct core.py ownership when possible

--- setup_python_rule_engine_as_only_rule_engine.py
+++ setup_python_rule_engine_as_only_rule_engine.py
@@ -1,13 +1,24 @@
 from __future__ import print_function
 
 import os
+import pwd
+import grp
 import shutil
 
 from irods.configuration import IrodsConfig
 from irods import paths
 
 def main():
-    shutil.copy(os.path.join(paths.config_directory(), 'core.py.template'), os.path.join(paths.config_directory(), 'core.py'))
+
+    src_core_py_tpl = os.path.join(paths.config_directory(), 'core.py.template')
+    dst_core_py = os.path.join(paths.config_directory(), 'core.py')
+    shutil.copy(src_core_py_tpl , dst_core_py)
+    if os.getuid() == 0:
+        try:
+            os.chown( dst_core_py,
+                      pwd.getpwnam('irods').pw_uid,
+                      grp.getgrnam('irods').gr_gid)
+        except KeyError: pass
 
     irods_config = IrodsConfig()
     irods_config.server_config['plugin_configuration']['rule_engines'] = [
