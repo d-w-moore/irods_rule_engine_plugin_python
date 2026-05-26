32ccaa1346438bc04232c0c2074dee7bac245bfb
d-w-moore <dmoore@renci.org>
2026-05-26 15:50:14 -0400

    Revert "fix options (case_sensitive particularly) sent to new Query"
    This reverts commit f2681fc3939e98f6ca496d084801fdfaf8914679.

--- genquery.py
+++ genquery.py
@@ -267,7 +267,8 @@ class Query(object):
                     # So instead, we run the query twice manually. This should
                     # perform only slightly worse.
                     # [1]: https://github.com/irods/irods/blob/4.2.6/plugins/database/src/general_query.cpp#L2393
-                    self._total = self.copy(limit=0, options=self.options|Option.RETURN_TOTAL_ROW_COUNT).total_rows()
+                    self._total = Query(self.callback, self.columns, self.conditions, limit=0,
+                                        options=self.options|Option.RETURN_TOTAL_ROW_COUNT).total_rows()
 
         return self._total
 
